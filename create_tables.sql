3c85e80d5bef72fff01e2d69d0a31838f99e80d7
David R <94697110+davidxxmm@users.noreply.github.com>
2021-12-03 07:23:49 -0300

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -3,3 +3,28 @@ CREATE TABLE public.sql_empresas (
 	nombre_empresa varchar NULL,
 	CONSTRAINT sql_empresas_pk PRIMARY KEY (id_empresa)
 );
+
+/////// crea tabla de usuarios
+
+CREATE TABLE public.sql_usuarios (
+	id_usuario int4 NOT NULL,
+	id_empresa int4 NOT NULL,
+	nombre varchar NULL,
+	CONSTRAINT sql_usuarios_pk PRIMARY KEY (id_usuario, id_empresa)
+);
+
+
+-- public.sql_usuarios foreign keys
+
+ALTER TABLE public.sql_usuarios ADD CONSTRAINT sql_usuarios_fk FOREIGN KEY (id_empresa) REFERENCES public.sql_empresas(id_empresa); 
+          
+        
+///// crear tabla de movimientos
+
+CREATE TABLE public.sql_movimientos (
+	id_movimiento serial4 NOT NULL,
+	id_usuario int4 NULL,
+	cantidad_ha int4 NULL,
+	creacion2 timestamp NULL,
+	CONSTRAINT sql_movimientos_pk PRIMARY KEY (id_movimiento)
+);
